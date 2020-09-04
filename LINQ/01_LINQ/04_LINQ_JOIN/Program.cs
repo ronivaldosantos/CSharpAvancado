1798b2c755576cbd5cbf29bb673d19b646d758ef
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-09-04 11:09:37 -0300

    complemento sistema

--- LINQ/01_LINQ/04_LINQ_JOIN/Program.cs
+++ LINQ/01_LINQ/04_LINQ_JOIN/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _04_LINQ_JOIN
 {
@@ -10,26 +8,29 @@ namespace _04_LINQ_JOIN
     {
         static void Main(string[] args)
         {
+            //Lista de livros
             List<Livro> listaLivro = new List<Livro>();
             listaLivro.Add(new Livro() { Id = 1, AutorId = 2, Titulo = "Amor amado" });
             listaLivro.Add(new Livro() { Id = 2, AutorId = 2, Titulo = "Bem amado" });
             listaLivro.Add(new Livro() { Id = 3, AutorId = 3, Titulo = "Um espião em Washigton" });
             listaLivro.Add(new Livro() { Id = 4, AutorId = 1, Titulo = "A vida na terra" });
 
+            //Lista de autores
             List<Autor> listaAutor = new List<Autor>();
             listaAutor.Add(new Autor() { Id = 1, Nome = "Leonardo" });
             listaAutor.Add(new Autor() { Id = 2, Nome = "Maria Maria"});
             listaAutor.Add(new Autor() { Id = 3, Nome = "Joseph" });
-                                                                                                                        //** Tipo anônimus **  
+
+            // ******* Utilizando LINQ e LAMBDA para junção de objetos através de JOIN. *******
+                                                                                                                      //** Tipo anônimus **  
             var listaJoin = from livros in listaLivro join autor in listaAutor on livros.AutorId equals autor.Id select new { livros,autor};
 
             foreach (var item in listaJoin)
             {
                 Console.WriteLine("Livro: " + item.livros.Titulo + " - Autor: " + item.autor.Nome);
             }
-
+                
             Console.ReadKey();
-
         }
     }
 }
---
+++ LINQ/01_LINQ/05_LINQ_GROUP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LINQ_GROUP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listaNum = { 1, 1, 1, 1, 4, 4, 2, 3, 5, 6, 6, 10, 9, 8 };
+
+            //Utilizando distinct
+            //var listaFiltrada = listaNum.Distinct().OrderBy(a=>a);
+
+            //Utilizando Goup By
+            var listaFiltrada = listaNum.OrderBy(a=>a).GroupBy(a => a).Select(a => a);
+
+            //GROUP / DISTINCT 
+            foreach (var item in listaFiltrada)
+            {
+                //Utilizando distinct    
+                //Console.WriteLine(item);
+
+                //Utilizando Goup By
+                Console.WriteLine(item.Key);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
